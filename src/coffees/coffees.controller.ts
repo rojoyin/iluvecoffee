66f890061f5482af87f009352c8cfe99653aad99
pao <pao.paty.chicaiza@gmail.com>
2022-11-16 18:05:51 -0500

    Add coffees controller

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action returns all the coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `this is the id ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+}

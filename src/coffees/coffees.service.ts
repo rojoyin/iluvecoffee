a7aaa717c4d67dd6a6e251c24eecfdebd46d7bdd
pao <pao.paty.chicaiza@gmail.com>
2022-11-17 19:45:15 -0500

    Add crud operations to coffee entity

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      brand: 'Shipwreck Roast',
+      name: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    return this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(updateCoffeeDto.id);
+    if (existingCoffee) {
+      // update the existing entry
+    }
+  }
+
+  remove(id: string) {
+    const existingCoffeeIndex = this.coffees.findIndex(
+      (item) => item.id === +id,
+    );
+    if (existingCoffeeIndex >= 0) {
+      this.coffees.splice(existingCoffeeIndex, 1);
+    }
+  }
+}

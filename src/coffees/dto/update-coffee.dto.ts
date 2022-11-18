f507b57985bbfb5bde25d4b662c6d04c58119a29
pao <pao.paty.chicaiza@gmail.com>
2022-11-18 16:34:06 -0500

    Refactor UpdateCoffeeDto class

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,6 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { CreateCoffeeDto } from './create-coffee.dto';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
+// PartialType from @nest/mapped-types library makes all the fields from the
+// class passed as parameter optional, and inherits the validation rules

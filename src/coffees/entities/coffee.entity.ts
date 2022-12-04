9351fe9c747b8343242655104880c91b13805f8e
pao <pao.paty.chicaiza@gmail.com>
2022-12-04 18:45:55 -0500

    Add event entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -18,6 +18,9 @@ export class Coffee {
   @Column()
   brand: string;
 
+  @Column({ default: 0 })
+  recommendations: number;
+
   @JoinTable() // owner side of the relationship
   @ManyToMany((type) => Flavor, (flavor) => flavor.coffees, { cascade: true })
   flavors: Flavor[];

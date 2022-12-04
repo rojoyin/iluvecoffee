a8ce7286414319c875dde8b8547e8630215795aa
Juan Carlos Jimenez <rojoyin@gmail.com>
2022-12-04 18:58:07 -0500

    Add recommend coffee feature (#2)
    * Add event entity
    
    * Add event entity
    
    * Add even table to db
    
    * Add reommend coffee migrations
    
    Co-authored-by: pao <pao.paty.chicaiza@gmail.com>

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,11 +4,12 @@ import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { Event } from '../events/entities/event.entity';
 
 @Module({
   controllers: [CoffeesController], // API routes we want this module to instantiate
   providers: [CoffeesService], // list of services that need to be instantiated by the nest injector. Any providers here will be available within THIS module itself, unless it is exported
   exports: [CoffeesService], // providers within THIS current module that will be available anywhere this module is imported
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor])], // list of modules that THIS module requires, any exported providers of the imported modules will be available within this module
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // list of modules that THIS module requires, any exported providers of the imported modules will be available within this module
 })
 export class CoffeesModule {}

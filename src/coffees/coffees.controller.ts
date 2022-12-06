859a23c822f8954596605502f38c523592768d49
rojoyin <rojoyin@gmail.com>
2022-12-06 18:39:53 -0500

    Inject REQUEST object to coffees contorller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
   Patch,
   Post,
@@ -12,10 +13,14 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { REQUEST } from '@nestjs/core';
 
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeeService: CoffeesService) {
+  constructor(
+    private readonly coffeeService: CoffeesService,
+    @Inject(REQUEST) private readonly request: Request, // using request scope providers may impact the application performance, as each request will instantiate the service // however, it provides more metadata about each request that arrives to the server
+  ) {
     console.log('CoffeesController created');
     // this shows that whenever a service this controller depends on and it is request scoped, the controller also becomes request scoped
   }

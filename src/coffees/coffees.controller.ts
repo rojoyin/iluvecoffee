8efb516992ee33c3d588ea9f76df9af1251b9d27
rojoyin <rojoyin@gmail.com>
2022-12-07 18:53:26 -0500

    Fix Request type import

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,6 +13,7 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { Request } from 'express';
 import { REQUEST } from '@nestjs/core';
 
 @Controller('coffees')
@@ -22,7 +23,7 @@ export class CoffeesController {
     @Inject(REQUEST) private readonly request: Request, // using request scope providers may impact the application performance, as each request will instantiate the service // however, it provides more metadata about each request that arrives to the server
   ) {
     console.log('CoffeesController created');
-    // this shows that whenever a service this controller depends on and it is request scoped, the controller also becomes request scoped
+    // this shows that whenever a service this controller depends on, and it is request scoped, the controller also becomes request scoped
   }
 
   @Get()

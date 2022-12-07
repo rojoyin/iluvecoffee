7e22ef7eed18c0f2969965bfe41fe4b4edba076a
Juan Carlos Jimenez <rojoyin@gmail.com>
2022-12-07 18:54:35 -0500

    Add config service (#5)
    * Install config dependency
    
    * Ignore env files
    
    * Get DB configuration variables from environment variables
    
    * Fix Request type import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'pass123', // user password
-      database: 'postgres', // name of our database,
+      host: process.env.DATABASE_HOST, // database host
+      port: +process.env.DATABASE_PORT, // database host
+      username: process.env.DATABASE_USER, // username
+      password: process.env.DATABASE_PASSWORD, // user password
+      database: process.env.DATABASE_NAME, // name of our database,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: false, // your entities will be synced with the database(recommended: disable in prod)
     }),

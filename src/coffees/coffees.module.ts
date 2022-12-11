c738dd2effcf5c5048cc6dc250e82de8b8152122
rojoyin <rojoyin@gmail.com>
2022-12-11 11:31:16 -0500

    Explore config service use

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,6 +6,7 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffe.constants';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [CoffeesController], // API routes we want this module to instantiate
@@ -19,6 +20,6 @@ import { COFFEE_BRANDS } from './coffe.constants';
     },
   ], // list of services that need to be instantiated by the nest injector. Any providers here will be available within THIS module itself, unless it is exported
   exports: [CoffeesService], // providers within THIS current module that will be available anywhere this module is imported
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // list of modules that THIS module requires, any exported providers of the imported modules will be available within this module
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule], // list of modules that THIS module requires, any exported providers of the imported modules will be available within this module
 })
 export class CoffeesModule {}

569c99f0a710614d7665f7481e17b5afab29cc35
rojoyin <rojoyin@gmail.com>
2022-12-11 11:21:23 -0500

    Add env vars schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,18 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.environment', // default .env at root. Also, an array of strings is possible
       ignoreEnvFile: false, // set to true if your env vars come from UI provider (heroku, etc)
+      validationSchema: Joi.object({
+        // default: all schema keys are optional
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({

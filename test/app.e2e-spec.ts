651d4432d75186a88b8ab213d5ba40340ec39fc2
rojoyin <rojoyin@gmail.com>
2023-01-23 18:43:03 -0500

    Fix e2e base route test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import * as process from "process";
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -11,13 +12,14 @@ describe('AppController (e2e)', () => {
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication(); // reference of an entire nest application
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
+      .set('Authorization', process.env.API_KEY) // set header, needed because of the api-key global guard we're using
       .expect(200)
       .expect('Hello World!');
   });

5a5469d6e34d29b3f650bb40727ab3e81a83f15e
rojoyin <rojoyin@gmail.com>
2023-01-23 19:36:13 -0500

    Add moduleFixture to e2e test case

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,8 +1,14 @@
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
 
 describe('[Feature] Coffees - /coffees', () => {
   let app: INestApplication;
   beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [CoffeesModule],
+    }).compile();
+    const app = moduleFixture.createNestApplication();
     await app.init();
   });
 
